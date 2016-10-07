0c1c662fd1090caac1357bf04d829b4c76228617
Sean Spradlin <seanspradlin@gmail.com>
2016-10-07 10:27:10 -0500

    made up and down function for paddles

--- app/scripts/main/paddle.ts
+++ app/scripts/main/paddle.ts
@@ -1,9 +1,22 @@
 namespace Main {
   export class Paddle extends Phaser.Graphics {
     constructor(game: BrowserPong, x: number, y: number) {
-      super(game);
+      super(game, x, y);
       this.beginFill(0x081820);
-      this.drawRect(x, y, 20, 100);
+      this.drawRect(0, 0, 10, 50);
+      game.add.existing(this);
+    }
+
+    up(): void {
+      if (this.y > 0) {
+        this.y--;
+      }
+    }
+
+    down(): void {
+      if (this.y < (this.game.stage.height - this.height)) {
+        this.y++;
+      }
     }
   }
 }
--- app/scripts/main/state.ts
+++ app/scripts/main/state.ts
@@ -1,7 +1,10 @@
 namespace Main {
   export class State extends Phaser.State {
+    leftPaddle: Paddle;
+
     create() {
       console.log('Game started');
+      this.leftPaddle = new Paddle(this.game, 10, 10);
     }
   }
 }
